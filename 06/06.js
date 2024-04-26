45488d20392b0e31621ee745d79f3d2cc49e3f44
llsy0101 <tpduddldia@naver.com>
2024-04-26 18:00:17 +0900

    0426.js(한번 더 해보기)

---
+++ 06/06.js
@@ -0,0 +1,21 @@
+/* 
+단위환산
+*/
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const sel1 = document.querySelector('#sel1');
+    const sel2 = document.querySelector('#sel2');
+
+    const txt1 = document.querySelector(#)
+
+    sel1.addEventListener('change', ()=>{
+        console.log(sel1.value, sel2.value) ;
+
+
+sel1.addEventListener('change',)
+
+});
+
+
+
+});
